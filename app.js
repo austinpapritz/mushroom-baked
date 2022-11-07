6174ebd7f2ab663cca83f3eb2132a13f3a61c674
Austin Papritz <austin@papritz.dev>
2022-11-07 12:47:43 -0800

    friend input adds friend object to array

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const friendInputEl = document.getElementById('friend-input');
 const mushroomsEl = document.querySelector('.mushrooms');
 const addMushroomButton = document.getElementById('add-mushroom-button');
 const addFriendButton = document.getElementById('add-friend-button');
+
 // initialize state
 
 let mushroomCount = 3;
@@ -42,10 +43,19 @@ addMushroomButton.addEventListener('click', () => {
 
 addFriendButton.addEventListener('click', () => {
     // get the name from the input
+    const value = friendInputEl.value;
+
     // create a new friend object
+    const newFriend = {
+        name: `${value}`,
+        satisfaction: 1,
+    };
     // push it into the friends state array, passed in as an argument
+    friendData.push(newFriend);
     // reset the input
+    friendInputEl.value = '';
     // display all the friends (use a function here)
+    displayFriends();
 });
 
 function displayFriends() {
@@ -54,13 +64,11 @@ function displayFriends() {
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
-
         // this is a clickable list, so . . .
         //     add an event listener to each friend
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
         //             increment the friends satisfaction and decrement your mushrooms
         //             then display your friends and mushrooms with the updated state
-
         // append the friendEl to the friends list in DOM
     }
 }
