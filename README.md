02593b3003ae766e50e625dd633fbe459e3a8118
Austin Papritz <austin@papritz.dev>
2022-11-07 15:07:18 -0800

    magic mushroom showing, but turning other mushrooms magic

--- README.md
+++ README.md
@@ -19,3 +19,7 @@
 | On clicking a friend, a mushroom should vanish from the table, and the friend should become more satisfied. Satisfaction level should be visible to the user as different emojis                                                     |   2 |
 | On clicking a friend, if that friend is completely satisfied, they can eat no more mushrooms. Also, if you try to feed a friend and there are no mushrooms, user should get an alert telling them to go forage for another mushroom. |   2 |
 | Click on the 'invite' button to create a new (unsatisfied) friend (with a random default name if none is provided) and add them to the page                                                                                          |   2 |
+
+# Magic Mushroom
+
+On addMushroomButton click, 1/3 chance of magic-mushroom.png appearing
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import functions and grab DOM elements
-import { renderMushroom, renderFriend } from './render-utils.js';
+import { renderMushroom, renderMagMushroom, renderFriend } from './render-utils.js';
 
 const friendsEl = document.querySelector('.friends');
 const friendInputEl = document.getElementById('friend-input');
@@ -31,11 +31,16 @@ const friendData = [
 ];
 
 addMushroomButton.addEventListener('click', () => {
-    if (Math.random() > 0.5) {
+    if (Math.random() > 0.9) {
         alert('found a mushroom!');
 
         mushroomCount++;
         displayMushrooms();
+    } else if (Math.random() > 0.1) {
+        alert('found a magic mushroom!!!');
+
+        mushroomCount++;
+        displayMagMushrooms();
     } else {
         alert('no luck!');
     }
@@ -60,20 +65,22 @@ addFriendButton.addEventListener('click', () => {
 
 function displayFriends() {
     // clear out the friends in DOM
-    friendsEl.textContent = '';
     // for each friend in state . . .
+    // use renderFriend to make a friendEl
+    // this is a clickable list, so . . .
+    //         and if the friend's satisfaction level is below 3 and you have mushrooms left
+    //     add an event listener to each friend
+    //             then display your friends and mushrooms with the updated state
+    friendsEl.textContent = '';
     for (let friend of friendData) {
-        // use renderFriend to make a friendEl
         const friendEl = renderFriend(friend);
-        // this is a clickable list, so . . .
-        //     add an event listener to each friend
-        friendEl.addEventListener('Click', () => {
-            //         and if the friend's satisfaction level is below 3 and you have mushrooms left
-
-            //             increment the friends satisfaction and decrement your mushrooms
-            friend.satisfaction++;
-
-            //             then display your friends and mushrooms with the updated state
+        friendEl.addEventListener('click', () => {
+            if (friend.satisfaction < 3 && mushroomCount > 0) {
+                friend.satisfaction++;
+                mushroomCount--;
+                displayMushrooms();
+                displayFriends();
+            }
         });
         friendsEl.append(friendEl);
         // append the friendEl to the friends list in DOM
@@ -86,6 +93,16 @@ function displayMushrooms() {
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
         const mushroom = renderMushroom(i);
+        mushroomsEl.append(mushroom);
+    }
+}
+
+function displayMagMushrooms() {
+    // clear out the mushroom div
+    mushroomsEl.textContent = '';
+    for (let i = 0; i < mushroomCount; i++) {
+        // for each mushroom in your mushroom state, render and append a mushroom
+        const mushroom = renderMagMushroom(i);
         mushroomsEl.append(mushroom);
     }
 }
